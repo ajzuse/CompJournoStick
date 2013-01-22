c2780045fadeaaa1d8e7a24a76b1a09eda2a688b
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2013-01-22 01:34:35 -0800

    placeholder - TODO list for packaging

---
+++ R-scripts/load-packages.R
@@ -0,0 +1,59 @@
+#
+# Copyright (C) 2012 by M. Edward (Ed) Borasky
+#
+# This program is licensed to you under the terms of version 3 of the
+# GNU Affero General Public License. This program is distributed WITHOUT
+# ANY EXPRESS OR IMPLIED WARRANTY, INCLUDING THOSE OF NON-INFRINGEMENT,
+# MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE. Please refer to the
+# AGPL (http://www.gnu.org/licenses/agpl-3.0.txt) for more details.
+#
+source("http://bioconductor.org/biocLite.R", echo=TRUE)
+biocLite('Rgraphviz')
+warnings()
+
+install.packages(
+  c(
+    'RSclient',
+    'Rserve',
+    'RUnit',
+    'inline',
+    'rbenchmark',
+    'Rcmdr',
+    'rattle',
+    'devtools',
+    'testthat',
+    'roxygen2',
+    'twitteR',
+    'seewave',
+    'tuneR',
+    'audio',
+    'ctv', 
+    'e1071',
+    'sem',
+    'relimp',
+    'multcomp',
+    'lmtest',
+    'leaps',
+    'effects',
+    'abind',
+    'RJSONIO',
+    'quantmod',
+    'shiny',
+    'markdown',
+    'RcmdrPlugin.TextMining',
+    'tm',
+    'tm.plugin.dc',
+    'tm.plugin.mail'
+  ),
+  repos='http://cran.fhcrc.org'
+)
+warnings()
+library(ctv)
+update.views(
+  c(
+    'ReproducibleResearch',
+    'Graphics'
+  ),
+  repos='http://cran.fhcrc.org'
+)
+warnings()
