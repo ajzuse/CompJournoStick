5c51e2b71213115e773f2451fa785069f64e534b
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2013-01-23 09:53:38 -0800

    I CAN HAZ STICK!

---
+++ LiveImageCreation/make-usb-stick.bash
@@ -0,0 +1,16 @@
+#! /bin/bash
+#
+# Copyright (C) 2013 by M. Edward (Ed) Borasky
+#
+# This program is licensed to you under the terms of version 3 of the
+# GNU Affero General Public License. This program is distributed WITHOUT
+# ANY EXPRESS OR IMPLIED WARRANTY, INCLUDING THOSE OF NON-INFRINGEMENT,
+# MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE. Please refer to the
+# AGPL (http://www.gnu.org/licenses/agpl-3.0.txt) for more details.
+#
+
+sudo livecd-iso-to-disk --format --reset-mbr --efi \
+  --overlay-size-mb 2047 \
+  --home-size-mb 2047 \
+  /opt/ROASt/ROASt.iso \
+  $1
