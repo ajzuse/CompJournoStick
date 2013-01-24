a894fc0e6bcd0b307a50b6c7203bfc9c658d2515
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2013-01-24 15:21:24 -0800

    the great merge!

--- FedoraInstall/install-usertools.bash
+++ FedoraInstall/install-usertools.bash
@@ -9,28 +9,46 @@
 # AGPL (http://www.gnu.org/licenses/agpl-3.0.txt) for more details.
 #
 
+./set-env.bash
+source ~/.bash_profile
+
 # Package groups
 sudo yum group install -y \
   gnome-desktop \
+  multimedia \
+  office \
   libreoffice \
   firefox \
-  development-tools
+  design-suite \
+  development-tools \
+  development-libraries \
+  virtualization \
+  books-and-guides \
+  mongodb \
+  sql-server \
+  fedora-packager
+
+# Developer packages
+  yum-utils \
+  livecd-tools \
+  spin-kickstarts \
+  liveusb-creator \
+  isomd5sum \
+  createrepo \
+  yumex \
+  R2spec
 
 # End-user packages
 sudo yum install -y \
+  xchat \
+  pidgin \
   aria2 \
   curl \
   wget \
-  xterm \
   vim-enhanced \
   vim-X11 \
   powertop \
-  kernel-tools \
-  audacity \
-  ggobi \
-  R \
-  antiword \
-  rkward
+  kernel-tools
 
 # Graphviz
 sudo yum install -y \
@@ -57,39 +75,165 @@ sudo yum install -y \
   QuantLib-test \
   QuantLib
 
-# Headers
+# PostgreSQL additions
 sudo yum install -y \
-  atlas-devel \
-  expat-devel \
-  fftw-devel \
-  fontconfig-devel \
-  freeglut-devel \
-  freetype-devel \
-  gd-devel \
-  ggobi-devel \
-  gmp-devel \
-  gsl-devel \
-  gtk2-devel \
-  gts-devel \
-  libcurl-devel \
-  libicu-devel \
-  libjpeg-turbo-devel \
-  libpng-devel \
-  libpng12 \
-  libsndfile-devel \
-  libtiff-devel \
-  libtool-ltdl-devel \
-  libXaw-devel \
-  libxml2-devel \
-  libXmu-devel \
-  libXt-devel \
-  mpfr-devel \
-  openjpeg-devel \
-  openssl-devel \
-  qtwebkit-devel \
-  R-devel \
-  readline-devel \
-  tcl-devel \
-  tk-devel
+  libreoffice-postgresql \
+  postgresql-contrib \
+  postgresql-devel \
+  postgresql-docs \
+  postgresql-odbc \
+  pgadmin3
 
-./install-rstudio.bash
+# eBook / Web / audio / photo / video editing
+sudo yum install -y \
+  inkscape-docs \
+  inkscape-sozi \
+  inkscape-view \
+  scribus-doc \
+  gtk-recordmydesktop \
+  dia-gnomeDIAicons \
+  audacity \
+  audacity-manual \
+  pitivi \
+  GREYCstoration-gimp \
+  gimp-data-extras \
+  gimp-devel-tools \
+  gimp-help \
+  gimp-help-browser \
+  gimp-lqr-plugin \
+  gimp-resynthesizer \
+  ufraw-gimp \
+  xsane-gimp \
+  gimp-fourier-plugin \
+  bluefish \
+  calibre \
+  freemind \
+  jabref \
+  lyx \
+  sigil
+
+# Scraping tools
+sudo yum install -y \
+  python-mechanize \
+  rubygem-mechanize \
+  perl-WWW-Mechanize \
+  perl-WWW-Mechanize-GZip \
+  ImageMagick \
+  ImageMagick-doc \
+  GraphicsMagick \
+  poppler-utils \
+  tesseract \
+  tesseract-devel \
+  tesseract-langpack-bul \
+  tesseract-langpack-cat \
+  tesseract-langpack-ces \
+  tesseract-langpack-chi_sim \
+  tesseract-langpack-chi_tra \
+  tesseract-langpack-dan \
+  tesseract-langpack-dan-frak \
+  tesseract-langpack-deu \
+  tesseract-langpack-deu-frak \
+  tesseract-langpack-ell \
+  tesseract-langpack-fin \
+  tesseract-langpack-fra \
+  tesseract-langpack-hun \
+  tesseract-langpack-ind \
+  tesseract-langpack-ita \
+  tesseract-langpack-jpn \
+  tesseract-langpack-kor \
+  tesseract-langpack-lav \
+  tesseract-langpack-lit \
+  tesseract-langpack-nld \
+  tesseract-langpack-nor \
+  tesseract-langpack-pol \
+  tesseract-langpack-por \
+  tesseract-langpack-ron \
+  tesseract-langpack-rus \
+  tesseract-langpack-slk \
+  tesseract-langpack-slv \
+  tesseract-langpack-spa \
+  tesseract-langpack-srp \
+  tesseract-langpack-swe \
+  tesseract-langpack-swe-frak \
+  tesseract-langpack-tgl \
+  tesseract-langpack-tur \
+  tesseract-langpack-ukr \
+  tesseract-langpack-vie
+
+# NoSQL
+sudo yum install -y \
+  desktopcouch \
+  desktopcouch-tools \
+  riak \
+  redis
+
+# Social Network Analysis
+sudo yum install -y \
+  python-networkx \
+  python-networkx-doc
+
+# Geospatial
+sudo yum install -y \
+  spatialite-gui \
+  spatialite-tools \
+  postgis \
+  postgis-docs \
+  postgis-utils \
+  pgRouting \
+  osm2pgsql \
+  qgis \
+  qgis-grass \
+  grass \
+  gdal \
+  gdal-doc \
+  proj
+
+# LibreOffice add-ons
+sudo yum install -y \
+  libreoffice-emailmerge \
+  libreoffice-graphicfilter \
+  libreoffice-headless \
+  libreoffice-impress \
+  libreoffice-langpack-en \
+  libreoffice-math \
+  libreoffice-nlpsolver \
+  libreoffice-ogltrans \
+  libreoffice-opensymbol-fonts \
+  libreoffice-pdfimport \
+  libreoffice-presentation-minimizer \
+  libreoffice-presenter-screen \
+  libreoffice-report-builder \
+  libreoffice-wiki-publisher \
+  libreoffice-writer2latex \
+  libreoffice-writer2xhtml \
+  libreoffice-xsltfilter \
+  ooo2gd
+
+# R and friends
+ggobi
+rkward
+R-abind
+R-biglm
+R-bigmemory
+R-bitops
+R-car
+R-combinat
+R-core
+R-DBI
+R-DynDoc
+R-java
+R-lmtest
+R-multcomp
+R-Rcompression
+R-RCurl
+R-rlecuyer
+R-RODBC
+R-RScaLAPACK
+R-RSQLite
+R-RUnit
+R-timeDate
+R-waveslim
+R-wavethresh
+R-XML
+R-xtable
+R-zoo
