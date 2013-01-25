eaf859e0530df39d5ecac0294eaa62b72cec001c
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2013-01-25 10:57:07 -0800

    tested on Fedora 18

--- RStudio/fedora-server-config.bash
+++ RStudio/fedora-server-config.bash
@@ -9,15 +9,14 @@
 # AGPL (http://www.gnu.org/licenses/agpl-3.0.txt) for more details.
 #
 
-useradd -r rstudio-server
-cp /usr/local/lib/rstudio-server/extras/pam/rstudio /etc/pam.d
-cp /usr/local/lib/rstudio-server/extras/init.d/redhat/rstudio-server /etc/init.d
-chmod +x /etc/init.d/rstudio-server
-/sbin/chkconfig --add rstudio-server
-ln -f -s /usr/local/lib/rstudio-server/bin/rstudio-server \
-  /usr/sbin/rstudio-server
-rstudio-server start
+sudo useradd -r rstudio-server
+sudo cp /usr/local/lib/rstudio-server/extras/pam/rstudio /etc/pam.d
+sudo cp /usr/local/lib/rstudio-server/extras/init.d/redhat/rstudio-server /etc/init.d
+sudo chmod +x /etc/init.d/rstudio-server
+sudo /sbin/chkconfig --add rstudio-server
+sudo ln -f -s /usr/local/lib/rstudio-server/bin/rstudio-server /usr/sbin/rstudio-server
+sudo rstudio-server start
 
 echo "Open TCP port 8787"
-system-config-firewall
+firewall-config
 firefox localhost:8787 &
