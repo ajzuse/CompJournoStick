e0ed41f9dc33fa5c388b69096db1551120f1a15c
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2013-01-26 18:20:58 -0800

    swap breaks it :-(

--- LiveImageCreation/make-usb-stick.bash
+++ LiveImageCreation/make-usb-stick.bash
@@ -12,7 +12,6 @@
 source ~/.bash_profile
 sudo livecd-iso-to-disk --format --reset-mbr --efi \
   --unencrypted-home \
-  --swap-size-mb 2047 \
   --overlay-size-mb 2047 \
   --home-size-mb 2047 \
   /opt/CompJournoStick/CompJournoStick.iso \
