cc5f239bea5d22708f9bd855dcf533fd5347d132
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2013-02-21 15:55:54 -0800

    unpack tarball

--- FedoraWorkstationInstall/get-latest-workbench.bash
+++ FedoraWorkstationInstall/get-latest-workbench.bash
@@ -1,13 +1,17 @@
 #! /bin/bash -v
 
-export WHERE=https://github.com/znmeb/Computational-Journalism-Publishers-Workbench/archive
 export VERSION=`curl \
   https://github.com/znmeb/Computational-Journalism-Publishers-Workbench/tags \
   | grep 'tar\.gz' \
   | head -n 1 \
   | sed 's:^.*archive/::' \
   | sed 's:\.tar\.gz.*$::'`
-echo ${VERSION}
-export WHAT=${VERSION}.tar.gz
+echo "Latest version is ${VERSION}"
 export PROJECT='Computational-Journalism-Publishers-Workbench'
-curl -L ${WHERE}/${WHAT} > ${PROJECT}-${WHAT}
+export WHERE=https://github.com/znmeb/Computational-Journalism-Publishers-Workbench/archive
+export WHAT=${VERSION}.tar.gz
+export DIR=${PROJECT}-${VERSION}
+export TARBALL=${DIR}.tar.gz
+curl -L ${WHERE}/${WHAT} > ${TARBALL}
+rm -fr ${DIR}
+tar xf ${TARBALL}
