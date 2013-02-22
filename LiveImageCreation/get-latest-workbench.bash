7bfa9a2b9e2c4969eb04af847c75cb9f0de31cfb
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2013-02-21 17:35:04 -0800

    mild refactoring

--- LiveImageCreation/get-latest-workbench.bash
+++ LiveImageCreation/get-latest-workbench.bash
@@ -4,14 +4,14 @@ export VERSION=`curl \
   https://github.com/znmeb/Computational-Journalism-Publishers-Workbench/tags \
   | grep 'tar\.gz' \
   | head -n 1 \
-  | sed 's:^.*archive/::' \
+  | sed 's:^.*archive/v::' \
   | sed 's:\.tar\.gz.*$::'`
 echo "Latest version is ${VERSION}"
 export PROJECT='Computational-Journalism-Publishers-Workbench'
-export WHERE=https://github.com/znmeb/Computational-Journalism-Publishers-Workbench/archive
-export WHAT=${VERSION}.tar.gz
-export DIR=${PROJECT}-${VERSION}
-export TARBALL=${DIR}.tar.gz
+export DIR="${PROJECT}-${VERSION}"
+export TARBALL="${DIR}.tar.gz"
+export WHERE='https://github.com/znmeb/Computational-Journalism-Publishers-Workbench/archive'
+export WHAT="v${VERSION}.tar.gz"
 curl -L ${WHERE}/${WHAT} > ${TARBALL}
 rm -fr ${DIR}
 tar xf ${TARBALL}
