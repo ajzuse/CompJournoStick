407667a1810b1512f6df8ebfa952f4b0b0502451
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2013-02-28 09:45:46 -0800

    collect installed packages and make RPMs from them!

---
+++ Fedora-RPMs/get-package-sources.R
@@ -0,0 +1,16 @@
+#
+# Copyright (C) 2012 by M. Edward (Ed) Borasky
+#
+# This program is licensed to you under the terms of version 3 of the
+# GNU Affero General Public License. This program is distributed WITHOUT
+# ANY EXPRESS OR IMPLIED WARRANTY, INCLUDING THOSE OF NON-INFRINGEMENT,
+# MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE. Please refer to the
+# AGPL (http://www.gnu.org/licenses/agpl-3.0.txt) for more details.
+#
+packages <- installed.packages()
+download.packages(
+  pkgs=packages[,1],
+  destdir='~/rpmbuild/SOURCES',
+  method='wget'
+)
+warnings()
