35c5067e509d7b30e9473baed5d17a3584022caf
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2013-07-07 10:58:46 -0700

    Flash now has its own script

---
+++ flash.bash
@@ -0,0 +1,17 @@
+#! /bin/bash
+#
+# Copyright (C) 2013 by M. Edward (Ed) Borasky
+#
+# This program is licensed to you under the terms of version 3 of the
+# GNU Affero General Public License. This program is distributed WITHOUT
+# ANY EXPRESS OR IMPLIED WARRANTY, INCLUDING THOSE OF NON-INFRINGEMENT,
+# MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE. Please refer to the
+# AGPL (http://www.gnu.org/licenses/agpl-3.0.txt) for more details.
+#
+
+# Adobe repositories
+sudo yum install -y \
+  http://linuxdownload.adobe.com/adobe-release/adobe-release-x86_64-1.0-1.noarch.rpm
+
+sudo yum install -y \
+  flash-plugin
