352de221281b2758429c1da55576bfada5f0069f
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2013-07-12 23:36:26 -0700

    script to save files destined for target ISO

---
+++ pack.bash
@@ -0,0 +1,21 @@
+#! /bin/bash
+#
+# Copyright (C) 2013 by M. Edward (Ed) Borasky
+#
+# This program is licensed to you under the terms of version 3 of the
+# GNU Affero General Public License. This program is distributed WITHOUT
+# ANY EXPRESS OR IMPLIED WARRANTY, INCLUDING THOSE OF NON-INFRINGEMENT,
+# MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE. Please refer to the
+# AGPL (http://www.gnu.org/licenses/agpl-3.0.txt) for more details.
+#
+
+# get rid of any garbage
+cd Scripts
+./cleanup.bash
+cd ..
+
+# now copy stuff to /opt so the kickstart file can find it
+sudo rm -fr /opt/Target; sudo mkdir -p /opt/Target
+sudo cp -a Scripts /opt/Target/Scripts
+sudo cp -a Doc /opt/Target/Doc
+sudo cp -a ~/R /opt/Target/R
