c384cce7e8ed2d66dbc167f3cb62b8344832184d
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2013-07-13 20:10:15 -0700

    put the video drivers back

--- Scripts/1SetUpWorkstation/1AddToDesktop.bash
+++ Scripts/1SetUpWorkstation/1AddToDesktop.bash
@@ -49,3 +49,35 @@ sudo yum install -y \
   libXt-devel \
   pandoc \
   pandoc-pdf
+
+# Video drivers - needed for some VM hosts
+sudo yum install -y \
+  xorg-x11-drv-apm \
+  xorg-x11-drv-ati \
+  xorg-x11-drv-cirrus \
+  xorg-x11-drv-dummy \
+  xorg-x11-drv-fbdev \
+  xorg-x11-drv-glint \
+  xorg-x11-drv-i128 \
+  xorg-x11-drv-i740 \
+  xorg-x11-drv-intel \
+  xorg-x11-drv-ivtv \
+  xorg-x11-drv-mach64 \
+  xorg-x11-drv-mga \
+  xorg-x11-drv-modesetting \
+  xorg-x11-drv-nouveau \
+  xorg-x11-drv-openchrome \
+  xorg-x11-drv-qxl \
+  xorg-x11-drv-r128 \
+  xorg-x11-drv-rendition \
+  xorg-x11-drv-s3virge \
+  xorg-x11-drv-savage \
+  xorg-x11-drv-siliconmotion \
+  xorg-x11-drv-sis \
+  xorg-x11-drv-sisusb \
+  xorg-x11-drv-tdfx \
+  xorg-x11-drv-trident \
+  xorg-x11-drv-v4l \
+  xorg-x11-drv-vesa \
+  xorg-x11-drv-vmware \
+  xorg-x11-drv-voodoo
