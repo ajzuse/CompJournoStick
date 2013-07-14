792b43d7c8ce73c4c1f51c22ac6ec0540de903f1
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2013-07-13 18:44:16 -0700

    stripping down for MATE desktop!

--- Scripts/1SetUpWorkstation/1AddToDesktop.bash
+++ Scripts/1SetUpWorkstation/1AddToDesktop.bash
@@ -17,116 +17,33 @@ sudo yum install -y \
 sudo yum clean all
 sudo yum update -y
 
-# R, development, live CD/DVD and USB tools
+# Git, createrepo, R, GGobi and RStudio
 sudo yum install -y \
-  gcc \
-  gcc-c++ \
-  gcc-gfortran \
   git-all \
-  fedora-packager \
-  yum-utils \
-  livecd-tools \
-  spin-kickstarts \
-  liveusb-creator \
-  isomd5sum \
   createrepo \
-  curl \
-  wget \
-  lynx \
-  aria2 \
-  fedora-review \
-  R2spec \
   R-java-devel \
-  ggobi-devel \
-  libcurl-devel \
-  libxml2-devel \
-  unixODBC-devel \
-  freeglut-devel \
-  libXt-devel \
-  mock
-
-# install new R2spec repository configuration file
-sudo cp repos.cfg /etc/R2spec/repos.cfg
-
-# make a place to build packages
-rpmdev-setuptree
+  ggobi-devel
 
 # create RStudio repository
 ./rstudio-repository.bash
 
+# install RStudio (Desktop)
+sudo yum install -y --nogpgcheck \
+  rstudio
+
 # Desktop basics that may not be there
 sudo yum install -y \
-  @libreoffice \
-  @firefox \
-  gnome-classic-session \
   vim-X11 \
   vim-enhanced \
   ntp \
   ntpdate \
   yumex
 
-# Perl Twitter tools
+# Dependencies for packages and task views
 sudo yum install -y \
-  perl-Net-Twitter \
-  perl-YAML \
-  perl-YAML-Parser-Syck \
-  perl-YAML-LibYAML \
-  perl-YAML-Syck \
-  perl-YAML-Tiny \
-  perl-JSON \
-  perl-JSON-XS \
-  perl-JSON-Any \
-  perl-JSON-PP \
-  perl-Hash-Flatten
-
-# Editing tools
-sudo yum install -y \
-  gtk-recordmydesktop \
-  lyx \
-  bluefish \
-  sigil \
-  calibre \
-  FlightCrew \
-  qtwebkit \
+  libcurl-devel \
+  libxml2-devel \
+  freeglut-devel \
+  libXt-devel \
   pandoc \
   pandoc-pdf
-
-# Guest tools - harmless on host!
-sudo yum install -y \
-  open-vm-tools-desktop
-
-# Video drivers - needed for some VM hosts
-sudo yum install -y \
-  xorg-x11-drv-apm \
-  xorg-x11-drv-ati \
-  xorg-x11-drv-cirrus \
-  xorg-x11-drv-dummy \
-  xorg-x11-drv-fbdev \
-  xorg-x11-drv-glint \
-  xorg-x11-drv-i128 \
-  xorg-x11-drv-i740 \
-  xorg-x11-drv-intel \
-  xorg-x11-drv-ivtv \
-  xorg-x11-drv-mach64 \
-  xorg-x11-drv-mga \
-  xorg-x11-drv-modesetting \
-  xorg-x11-drv-nouveau \
-  xorg-x11-drv-openchrome \
-  xorg-x11-drv-qxl \
-  xorg-x11-drv-r128 \
-  xorg-x11-drv-rendition \
-  xorg-x11-drv-s3virge \
-  xorg-x11-drv-savage \
-  xorg-x11-drv-siliconmotion \
-  xorg-x11-drv-sis \
-  xorg-x11-drv-sisusb \
-  xorg-x11-drv-tdfx \
-  xorg-x11-drv-trident \
-  xorg-x11-drv-v4l \
-  xorg-x11-drv-vesa \
-  xorg-x11-drv-vmware \
-  xorg-x11-drv-voodoo
-
-# RStudio (Desktop)
-sudo yum install -y --nogpgcheck \
-  rstudio
