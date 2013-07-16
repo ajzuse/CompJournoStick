e6a30fd021a6c8b20bb380fca007b0822b2853b9
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2013-07-15 17:45:34 -0700

    more trimming

--- Scripts/1SetUpWorkstation/1SudoInstallTools.bash
+++ Scripts/1SetUpWorkstation/1SudoInstallTools.bash
@@ -46,67 +46,9 @@ yum install -y \
   openssl-devel \
   pam-devel
 
-exit
-# Dependencies for packages and task views
+# knitr
 yum install -y \
-  libcurl-devel \
-  libxml2-devel \
-  freeglut-devel \
-  libXt-devel \
-  bwidget \
-  pandoc \
-  pandoc-pdf \
-  gdal-devel \
-  gdal-doc \
-  geos-devel \
-  proj-devel \
-  proj-epsg \
-  armadillo-devel \
-  poppler-devel \
-  freexl-devel \
-  libwebp-devel \
-  expat-devel \
-  xerces-c-devel \
-  libjpeg-turbo-devel \
-  jasper-devel \
-  netcdf-devel \
-  hdf5-devel \
-  libgeotiff-devel \
-  cfitsio-devel \
-  unixODBC-devel \
-  gsl-devel
-
-# Video drivers - needed for some VM hosts
-yum install -y \
-  xorg-x11-drv-apm \
-  xorg-x11-drv-ati \
-  xorg-x11-drv-cirrus \
-  xorg-x11-drv-dummy \
-  xorg-x11-drv-fbdev \
-  xorg-x11-drv-glint \
-  xorg-x11-drv-i128 \
-  xorg-x11-drv-i740 \
-  xorg-x11-drv-intel \
-  xorg-x11-drv-ivtv \
-  xorg-x11-drv-mach64 \
-  xorg-x11-drv-mga \
-  xorg-x11-drv-modesetting \
-  xorg-x11-drv-nouveau \
-  xorg-x11-drv-openchrome \
-  xorg-x11-drv-qxl \
-  xorg-x11-drv-r128 \
-  xorg-x11-drv-rendition \
-  xorg-x11-drv-s3virge \
-  xorg-x11-drv-savage \
-  xorg-x11-drv-siliconmotion \
-  xorg-x11-drv-sis \
-  xorg-x11-drv-sisusb \
-  xorg-x11-drv-tdfx \
-  xorg-x11-drv-trident \
-  xorg-x11-drv-v4l \
-  xorg-x11-drv-vesa \
-  xorg-x11-drv-vmware \
-  xorg-x11-drv-voodoo
+  libcurl-devel
 
 # Editing
 yum install -y \
