dec31d3d2a1b9aa3e7d56e1e70ae712778be08de
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2013-07-16 22:31:52 -0700

    step numbers removed!

--- Scripts/MakeLiveISOImage/list-packages.pl
+++ Scripts/MakeLiveISOImage/list-packages.pl
@@ -9,7 +9,7 @@
 # AGPL (http://www.gnu.org/licenses/agpl-3.0.txt) for more details.
 #
 
-&listPackages('../1SetUpWorkstation/1SudoInstallTools.bash');
+&listPackages('../SetUpWorkstation/SudoInstallTools.bash');
 exit;
 
 sub listPackages {
