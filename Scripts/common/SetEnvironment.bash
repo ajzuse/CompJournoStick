fb08dadebf1f25c77e2e97fb8d5cd1516ee6f942
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2013-07-17 11:24:35 -0700

    script to set environment files

---
+++ Scripts/common/SetEnvironment.bash
@@ -0,0 +1,14 @@
+#! /bin/bash
+#
+# Copyright (C) 2013 by M. Edward (Ed) Borasky
+#
+# This program is licensed to you under the terms of version 3 of the
+# GNU Affero General Public License. This program is distributed WITHOUT
+# ANY EXPRESS OR IMPLIED WARRANTY, INCLUDING THOSE OF NON-INFRINGEMENT,
+# MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE. Please refer to the
+# AGPL (http://www.gnu.org/licenses/agpl-3.0.txt) for more details.
+#
+
+cp bashrc ~/.bashrc
+cp bash_profile ~/.bash_profile
+cp Rprofile ~/.Rprofile
